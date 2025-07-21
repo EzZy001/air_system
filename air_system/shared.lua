0d0c6d196fc4903280f4d4f48f631b8be4a74007
EzZy001 <120203312+EzZy001@users.noreply.github.com>
2025-07-21 10:22:05 +0200

    upload folder

---
+++ air_system/shared.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config.settings = {
+    language = 'en',
+}
+
+Config.NPC = {
+    TheoryNPC = { coords = vector3(-1156.187, -2717.309, 19.88733), heading = 295.8031, 
+    --animdict =  "idle_a", animname = "idle_a",
+    pedmodel = "a_f_m_bevhills_02",      
+    --[[ 
+        prop = {
+            model = "p_amb_clipboard_01",
+            bone = 18905, -- PH_L_Hand
+            pos = vector3(0.13, 0.02, 0.02),
+            rot = vector3(0.0, 0.0, 180.0)
+        } ]]--
+    }
+}
+
